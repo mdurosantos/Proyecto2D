4bd8aa5170f61c95f7fccf1a5a447393a516f66d
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-04-17 14:00:27 +0200

    Rotación de enemigo al patrullar y collider básico para detectar player

---
+++ Assets/Scripts/EnemyVisionCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVisionCollider : MonoBehaviour
+{
+    private bool playerInRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+            Debug.Log("Player left");
+        }
+    }
+}
--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -27,9 +27,11 @@ public class Patrol : MonoBehaviour
     void Update()
     {
 
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime); //Para desplazarse hacia el punto que toca.
 
-        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        
+        
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f) //Esperar
         {
             if(waitTime <= 0)
             {
@@ -41,6 +43,17 @@ public class Patrol : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
+        else
+        {
+            RotateToDirection();
+        }
+    }
+
+    void RotateToDirection()
+    {
+        Vector3 dir = transform.position - moveSpots[randomSpot].position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle-90f, Vector3.forward), 5.0f * Time.deltaTime);
     }
 
 }
