43ad9407db7c0960524662f20c578f5ad14b2be6
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-06-04 13:28:54 +0200

    Audio WIP

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioClip step;
+    static AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //step = Resources.Load<AudioClip>("Audio/player_footsteps/player_footsteps_");
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "step":
+                Debug.Log("STEP");
+                audioSrc.PlayOneShot(Resources.Load<AudioClip>("Audio/player_footsteps/player_footsteps_"+ Random.Range(1, 18)), 1f);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAudioEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioEvents : MonoBehaviour
+{
+    void PlayStep()
+    {
+        AudioManager.PlaySound("step");
+    }
+}
