117b9ec840f1e13846555888b4e68a15056f487a
Pol420 <pfernandezgu@edu.tecnocampus.cat>
2020-06-13 21:28:08 +0200

    stuff again

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioClip [] step;
+    public static AudioClip step;
     static AudioSource audioSrc;
     // Start is called before the first frame update
     void Start()
     {
-        step = new AudioClip[17];
-        for (int i = 0; i<17; i++)
-        {
-            step[i]= Resources.Load<AudioClip>("Audio/player_footsteps/player_footsteps_"+(i+1));
-        }
+        //step = Resources.Load<AudioClip>("Audio/player_footsteps/player_footsteps_");
         audioSrc = GetComponent<AudioSource>();
     }
 
@@ -23,11 +19,7 @@ public class AudioManager : MonoBehaviour
         switch (clip)
         {
             case "step":
-<<<<<<< HEAD
                 audioSrc.PlayOneShot(Resources.Load<AudioClip>("Audio/player_footsteps/player_footsteps_"+ Random.Range(1, 18)), 0.5f);
-=======
-                audioSrc.PlayOneShot(step[Random.Range(0, 17)], 0.5f);
->>>>>>> 2f140aa9cb843014ed7937726695890464209885
                 break;
         }
     }
