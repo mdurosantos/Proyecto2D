0d7cf69380dcd2da8f976745e0bee0dec8d4114f
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-06-16 16:11:15 +0200

    Cosas de la escena de inicio

--- Assets/Scripts/Game/StartSceneController.cs
+++ Assets/Scripts/Game/StartSceneController.cs
@@ -6,18 +6,20 @@ public class StartSceneController : MonoBehaviour
 {
     [SerializeField]
     private string firstLevel;
-    
+    /*private SpriteRenderer loading;
 
+    private void Start()
+    {
+        loading = GameObject.FindGameObjectWithTag("loading").GetComponent<SpriteRenderer>();
+    }*/
 
     void Update()
     {
         if (Input.GetButton("Interact")) {
+            //loading.enabled = true;
             SceneController.LoadNextScene();
         }
     }
 
-    private bool SpacePressed()
-    {
-        return Input.GetButton("Interact");
-    }
+
 }
