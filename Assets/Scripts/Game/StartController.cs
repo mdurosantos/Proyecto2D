24edfad39544908870260ea339e6d0be6c35583b
Manuel Duro <mduro@edu.tecnocampus.cat>
2020-06-19 11:04:58 +0200

    Escena de inicio cambiada

---
+++ Assets/Scripts/Game/StartController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    public static StartController Instance;
+    private static bool cursorInResume = true;
+    private CanvasGroup resume;
+    private CanvasGroup quit;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        resume = GameObject.FindGameObjectWithTag("Resume").GetComponent<CanvasGroup>();
+        quit = GameObject.FindGameObjectWithTag("Quit").GetComponent<CanvasGroup>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            AudioManager.PlaySound("okUI");
+            if (cursorInResume)
+            {
+                SceneController.LoadNextScene();
+            }
+            else
+            {
+                Debug.Log("Quit");
+                Application.Quit();
+
+            }
+        }
+        else if (Input.GetButtonDown("Vertical"))
+        {
+            MoveCursor();
+            CheckCursor();
+        }
+    }
+
+    private void MoveCursor()
+    {
+        AudioManager.PlaySound("moveUI");
+        cursorInResume = !cursorInResume;
+    }
+
+    private void CheckCursor()
+    {
+        if (cursorInResume)
+        {
+            resume.alpha = 1;
+            quit.alpha = 0;
+        }
+        else
+        {
+            resume.alpha = 0;
+            quit.alpha = 1;
+        }
+    }
+}
+
+
+
+    
+
